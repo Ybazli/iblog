969c7113934cfcfee1b9287804b1afd18bb88f59
Yashar Bazli <yashar.bazli@gmail.com>
2019-05-19 09:17:59 +0300

    working on iblog install command

--- app/Console/Commands/iBlogInstallCommand.php
+++ app/Console/Commands/iBlogInstallCommand.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\User;
+use http\Env;
 use Illuminate\Console\Command;
 
 class iBlogInstallCommand extends Command
@@ -38,38 +39,61 @@ class iBlogInstallCommand extends Command
      */
     public function handle()
     {
-        //ask database details for create .env file
-        $env = file_get_contents('.env.example');
-        dd(explode(' ' , $env));
-        $database['name'] = $this->ask('Database Name ?');
-        $database['username'] = $this->ask('Database User ?');
-        $database['password'] = $this->secret('Database Password ?');
-
-        \Artisan::call('migrate:fresh');
-
-        $this->info('The Database was migrated successfully.');
-
-        $data['first_name'] = $this->ask('What is your first name?');
-        $data['last_name'] = $this->ask('What is your last name?');
-        $data['email'] = $this->ask('What is your email?');
-        $data['username'] = $this->ask('What is your username?');
-        $data['password'] = $this->secret('What is your password?');
-
-        $user = factory(User::class)->create([
-            'first_name' => $data['first_name'],
-            'last_name' => $data['last_name'],
-            'username' => $data['username'],
-            'email' => $data['email'],
-            'password' => bcrypt($data['password'])
-        ]);
-        $this->info('Your user was created!');
-        //most the user added to admin list
-
+        $this->setPrefixForBlog();
+        $this->setPrefixForAdmin();
+        $this->info('All set! Enjoy that ;)');
     }
 
 
-    protected function createEnvFile(array $databse)
+    protected function setEnvironmentValue(array $values)
     {
 
+        $envFile = app()->environmentFilePath();
+        $str = file_get_contents($envFile);
+
+        if (count($values) > 0) {
+            foreach ($values as $envKey => $envValue) {
+
+                $str .= "\n";
+                $keyPosition = strpos($str, "{$envKey}=");
+                $endOfLinePosition = strpos($str, "\n", $keyPosition);
+                $oldLine = substr($str, $keyPosition, $endOfLinePosition - $keyPosition);
+
+                // If key does not exist, add it
+                if (!$keyPosition || !$endOfLinePosition || !$oldLine) {
+                    $str .= "{$envKey}={$envValue}\n";
+                } else {
+                    $str = str_replace($oldLine, "{$envKey}={$envValue}", $str);
+                }
+
+            }
+        }
+
+        $str = substr($str, 0, -1);
+        if (!file_put_contents($envFile, $str)) return false;
+        return true;
+
+    }
+
+    protected function setPrefixForBlog()
+    {
+        $answer = $this->ask('Enter your blog prefix' , 'blog');
+        $this->setEnvironmentValue(['URL_BLOG_PREFIX' => $answer]);
+
+    }
+
+    protected function setPrefixForAdmin()
+    {
+        $answer = $this->ask('Enter your admin side prefix' , 'admin');
+        $this->setEnvironmentValue(['URL_ADMIN_PREFIX' => $answer]);
+
+    }
+
+    protected function seedDataBase()
+    {
+        $answer = $this->ask('Do you want to seed some dummy data in DB ? if not leave blank.' , 'no');
+        if($answer != 'no'){
+            
+        }
     }
 }
