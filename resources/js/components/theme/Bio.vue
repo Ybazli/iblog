692aae8f260db71e3fc85220851cd215779ead0f
Yashar Bazli <yashar.bazli@gmail.com>
2019-05-24 10:52:23 +0300

    reset api commit

---
+++ resources/js/components/theme/Bio.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="border-b-1 border-grey-lighter  py-4">
+        <div class="font-serif text-grey-darker flex">
+
+            <img src="/users/ybazli.jpeg" 
+            class="rounded-full h-16 w-16 mr-2 mt-1  align-middle">
+
+            <p class=" leading-normal">Im a Software Engineer. Im a fulltime PHP& JavaScript Developer and part time Youtuber.<br>
+            I like using Laravel and Vue.
+            You can follow me on <a href="#" class="no-underline text-teal hover:underline">Github</a> or on 
+            <a href="#" class="no-underline text-teal hover:underline">Twitter</a>.</p>
+
+        </div>
+    </div>
+</template>
+
+<script>
+	export default {
+	}
+</script>
---
+++ resources/js/components/theme/Home.vue
@@ -0,0 +1,63 @@
+<template>
+	<div class="mt-10">
+        <h4 class="text-5xl text-grey-darkest montserrat">Yashar Bazli</h4>
+        <bio></bio>
+	    <!-- <div v-for="article in articles" 
+	    	class="my-5 border-b-1 border-grey-lighter pb-4 ">
+	    	<h2 class="text-xl">
+            <router-link 
+				class="text-grey-darker no-underline hover:text-teal-light"
+				:to="articleSlug(article.slug)" >
+                {{ article.title }}
+            </router-link>
+	    	</h2>
+	    	<div class="text-grey-dark italic text-xs mt-2">
+	    		{{ articleDate(article.created_at) }}
+	    	</div>
+            <div class="mt-5 text-xs">
+                <router-link 
+                    class="text-teal no-underline hover:text-teal-light"
+                    :to="articleSlug(article.slug)" >
+                        Read More →
+                </router-link>
+            </div>
+	    	
+	    </div> -->
+        <div class="flex justify-between mb-5 border-b-1 border-grey-lighter pb-5 text-xs">
+            <button 
+				class="border-1 text-grey-dark p-2 rounded hover:border-teal hover:text-teal">
+				Previos Page
+			</button>
+            <button 
+				class="border-1 text-grey-dark p-2 rounded hover:border-teal hover:text-teal">
+				Next Page
+			</button>
+        </div>
+    </div>
+</template>
+
+<script>
+import moment from 'moment';
+moment().format();
+    export default {
+    	data(){
+    		return{
+    			articles:[]
+    		}
+    	},
+    	mounted(){
+    		axios.get('/articles').then((response) =>{
+				console.log(JSON.parse(response.data));
+			});
+    	},
+    	methods:{
+    		articleDate(created_at){
+    			return moment(created_at, "YYYYMMDD").format("YYYY/MM/DD");
+    		},
+    		articleSlug(slug){
+    			return '/'+slug;
+    		}
+
+    	}
+    }
+</script>
